f396e612a9806e28dc62f2515606b84409fffa3a
Blaise Laflamme <blaise@laflamme.org>
2017-01-28 12:09:26 -0500

    initial blinker integration

---
+++ more/signals/directives.py
@@ -0,0 +1,31 @@
+from uuid import uuid4
+
+from blinker import Namespace
+from dectate import Action
+
+
+class SignalAction(Action):
+    config = {
+       'signal_registry': Namespace
+    }
+
+    def __init__(self, name):
+        self.name = name
+
+    def identifier(self, signal_registry):
+        return (uuid4().hex, self.name)
+
+    def perform(self, obj, signal_registry):
+        pass
+
+
+class ConnectSignalAction(SignalAction):
+
+    def perform(self, obj, signal_registry):
+        signal_registry.signal(self.name).connect(obj)
+
+
+class DisconnectSignalAction(SignalAction):
+
+    def perform(self, obj, signal_registry):
+        signal_registry.signal(self.name).disconnect(obj)
--- more/signals/main.py
+++ more/signals/main.py
@@ -1,5 +1,20 @@
+from dectate import directive
 import morepath
+
+from .directives import (
+    ConnectSignalAction,
+    DisconnectSignalAction
+)
 
 
 class SignalApp(morepath.App):
-    pass
+
+    connect = directive(ConnectSignalAction)
+    disconnect = directive(DisconnectSignalAction)
+
+    @property
+    def signals(self):
+        return self.config.signal_registry
+
+    def signal(self, event):
+        return self.signals.signal(event)
