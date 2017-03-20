3ae9d695cf233b46c6336a67f0bb1650c331be8c
Blaise Laflamme <blaise@laflamme.org>
2017-03-19 23:25:06 -0400

    add doc test

--- more/signals/tests/test_signals.py
+++ more/signals/tests/test_signals.py
@@ -49,3 +49,14 @@ def test_disconnect_signal_app():
     app = App()
     c = Client(app)
     c.get('/hello', status=200)
+
+
+def test_register_signal_with_doc_app():
+    class App(SignalApp):
+        pass
+
+    app = App()
+    app.commit()
+    app.signal(name='hello', doc='hello signal doc')
+
+    assert app.signals['hello'].__doc__ == 'hello signal doc'
