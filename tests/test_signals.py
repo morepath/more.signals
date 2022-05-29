69a240d59210c685aa1a3ff04abbcd191b76cbb4
Jürgen Gmach <juergen.gmach@googlemail.com>
2022-05-29 11:38:33 +0200

    Remove pydandoc
    This is no longer necessary as PyPI supports markdown directly.

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -7,48 +7,45 @@ def test_connect_signal_app():
     class App(SignalApp):
         pass
 
-    @App.path(path='/hello')
-    class Model(object):
+    @App.path(path="/hello")
+    class Model:
         pass
 
-    @App.connect('hello')
+    @App.connect("hello")
     def say_hello(sender, **data):
-        print('HELLO {}!'.format(data.get('name')))
+        print("HELLO {}!".format(data.get("name")))
 
     @App.json(model=Model)
     def hello_view(self, request):
-        request.app.signal('hello').send(
-            self,
-            name='Foo Bar'
-        )
+        request.app.signal("hello").send(self, name="Foo Bar")
         return {}
 
     app = App()
     c = Client(app)
-    c.get('/hello', status=200)
+    c.get("/hello", status=200)
 
 
 def test_disconnect_signal_app():
     class App(SignalApp):
         pass
 
-    @App.path(path='/hello')
-    class Model(object):
+    @App.path(path="/hello")
+    class Model:
         pass
 
-    @App.connect('hello')
+    @App.connect("hello")
     def say_hello(sender, **data):
-        print('HELLO {}!'.format(data.get('name')))
+        print("HELLO {}!".format(data.get("name")))
 
     @App.json(model=Model)
     def hello_view(self, request):
         return {}
 
-    App.disconnect('hello')(say_hello)
+    App.disconnect("hello")(say_hello)
 
     app = App()
     c = Client(app)
-    c.get('/hello', status=200)
+    c.get("/hello", status=200)
 
 
 def test_register_signal_with_doc_app():
@@ -57,6 +54,6 @@ def test_register_signal_with_doc_app():
 
     app = App()
     app.commit()
-    app.signal(name='hello', doc='hello signal doc')
+    app.signal(name="hello", doc="hello signal doc")
 
-    assert app.signals['hello'].__doc__ == 'hello signal doc'
+    assert app.signals["hello"].__doc__ == "hello signal doc"
