aaf26cb35a7969431f0405a94e17972aa9abfaa4
Jürgen Gmach <juergen.gmach@googlemail.com>
2022-05-29 11:26:04 +0200

    Modernize package
    - fix test setup
    - add linting via pre-commit
    - move package into src directory
    - add support for current Python versions
    - drop support for Python 2
    - run ci via gh actions

--- setup.py
+++ setup.py
@@ -4,84 +4,58 @@ more.signals
 Signals for Morepath.
 """
 
-import io
-from setuptools import (
-    setup,
-    find_packages
-)
+from setuptools import setup, find_packages
 
 try:
     import pypandoc
 except ImportError:
-    print('pypandoc not found, could not convert Markdown to RST')
+    print("pypandoc not found, could not convert Markdown to RST")
     pypandoc = None
 
 
 def read_md(f):
     if pypandoc:
-        return pypandoc.convert(f, 'rst')
-    return io.open(f, encoding='utf-8').read()
+        return pypandoc.convert(f, "rst")
+    return open(f, encoding="utf-8").read()
 
 
-version = '0.2.0-dev0'
+version = "0.2.0-dev0"
 
-long_description = '\n'.join((
-    read_md('README.md'),
-    read_md('CHANGES.md')
-))
+long_description = "\n".join((read_md("README.md"), read_md("CHANGES.md")))
 
-install_requires = [
-    'morepath',
-    'blinker'
-]
+install_requires = ["morepath", "blinker"]
 
-tests_require = [
-    'pytest',
-    'coverage',
-    'pytest-cov',
-    'webtest'
-]
+tests_require = ["pytest", "coverage", "pytest-cov", "webtest"]
 
-docs_require = [
-    'sphinx',
-    'docutils'
-]
+docs_require = ["sphinx", "docutils"]
 
-pypi_require = [
-    'pypandoc',
-    'twine'
-]
+pypi_require = ["pypandoc", "twine"]
 
 
 setup(
-    name='more.signals',
+    name="more.signals",
     version=version,
-    url='https://github.com/morepath/more.signals',
-    license='BSD',
-    author='Blaise Laflamme',
-    author_email='blaise@laflamme.org',
-    description='Signals for Morepath',
+    url="https://github.com/morepath/more.signals",
+    license="BSD",
+    author="Blaise Laflamme",
+    author_email="blaise@laflamme.org",
+    description="Signals for Morepath",
     long_description=long_description,
-    keywords='morepath events signals blinker',
+    keywords="morepath events signals blinker",
     classifiers=[
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
-    namespace_packages=['more'],
-    packages=find_packages(),
+    namespace_packages=["more"],
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    platforms='any',
+    platforms="any",
     install_requires=install_requires,
     tests_require=tests_require,
-    extras_require={
-        'test': tests_require,
-        'docs': docs_require,
-        'pypi': pypi_require
-    },
-    test_suite='more.signals'
+    extras_require={"test": tests_require, "docs": docs_require, "pypi": pypi_require},
+    test_suite="more.signals",
 )
